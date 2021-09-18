4a7cef146af74dd7dd1a2e14917698b1941133d6
Willem Veerman <willem.veerman@digital.homeoffice.gov.uk>
2021-09-18 14:15:22 +0100

    fix logstash perms, add proxy toggle

--- cluster.tf
+++ cluster.tf
@@ -3,14 +3,14 @@ resource "aws_elasticsearch_domain" "cluster" {
   elasticsearch_version = var.elasticsearch_version
 
   cluster_config {
-    instance_type            = var.data_instance_type
-    instance_count           = var.data_instance_count
+    instance_type  = var.data_instance_type
+    instance_count = var.data_instance_count
 
     dedicated_master_enabled = var.master_enabled
     dedicated_master_count   = var.master_instance_count
     dedicated_master_type    = var.master_instance_type
-    
-    zone_awareness_enabled   = true
+
+    zone_awareness_enabled = true
     zone_awareness_config {
       availability_zone_count = 3
     }
@@ -40,19 +40,19 @@ resource "aws_elasticsearch_domain" "cluster" {
   }
 
   log_publishing_options {
-    log_type = "ES_APPLICATION_LOGS"
-    enabled = true
+    log_type                 = "ES_APPLICATION_LOGS"
+    enabled                  = true
     cloudwatch_log_group_arn = aws_cloudwatch_log_group.es_domain_log_group.arn
   }
 
   log_publishing_options {
-    log_type = "INDEX_SLOW_LOGS"
-    enabled = true
+    log_type                 = "INDEX_SLOW_LOGS"
+    enabled                  = true
     cloudwatch_log_group_arn = aws_cloudwatch_log_group.es_domain_log_group.arn
   }
 
   domain_endpoint_options {
-    enforce_https = true
+    enforce_https       = true
     tls_security_policy = "Policy-Min-TLS-1-2-2019-07"
   }
 
@@ -66,7 +66,7 @@ resource "aws_elasticsearch_domain" "cluster" {
     internal_user_database_enabled = true
 
     master_user_options {
-      master_user_name = var.master_user_name
+      master_user_name     = var.master_user_name
       master_user_password = var.master_user_password
     }
   }
@@ -112,7 +112,7 @@ resource "null_resource" "create_local_users" {
   }
 
   provisioner "local-exec" {
-    command = "./${local_file.script[count.index].filename}"
+    command     = "./${local_file.script[count.index].filename}"
     interpreter = ["sh"]
   }
 
@@ -122,21 +122,21 @@ resource "null_resource" "create_local_users" {
 }
 
 resource "local_file" "script" {
-    count = var.admin_users != [] ? 1 : 0
-    content  = templatefile("${path.module}/files/create_admin_users.tmpl", 
-                {
-                  es_user=var.master_user_name,
-                  es_pass=var.master_user_password,
-                  aws_es_endpoint=aws_elasticsearch_domain.cluster.endpoint,
-                  admin_users=var.admin_users
-                })
-    filename = "${path.module}/${var.domain_name}-admin-user-script.sh"
+  count = var.admin_users != [] ? 1 : 0
+  content = templatefile("${path.module}/files/create_admin_users.tmpl",
+    {
+      es_user         = var.master_user_name,
+      es_pass         = var.master_user_password,
+      aws_es_endpoint = aws_elasticsearch_domain.cluster.endpoint,
+      admin_users     = var.admin_users
+  })
+  filename = "${path.module}/${var.domain_name}-admin-user-script.sh"
 }
 
 resource "null_resource" "bootstrap_service_users" {
 
   provisioner "local-exec" {
-    command = "./${local_file.service_users_script.filename}"
+    command     = "./${local_file.service_users_script.filename}"
     interpreter = ["sh"]
   }
 
@@ -146,32 +146,32 @@ resource "null_resource" "bootstrap_service_users" {
 }
 
 resource "local_file" "service_users_script" {
-    content  = templatefile("${path.module}/files/bootstrap_service_users.tmpl", 
-                {
-                  es_user=var.master_user_name,
-                  es_pass=var.master_user_password,
-                  aws_es_endpoint=aws_elasticsearch_domain.cluster.endpoint,
-                  logstash_username=var.logstash_username,
-                  logstash_password=var.logstash_password,
-                  logstash_permissions=var.logstash_permissions,
-                  proxy_cluster_permissions=var.proxy_cluster_permissions,
-                  proxy_username=var.proxy_username,
-                  proxy_password=var.proxy_password,
-                })
-    filename = "${path.module}/${var.domain_name}-bootstrap-service-script.sh"
+  content = templatefile("${path.module}/files/bootstrap_service_users.tmpl",
+    {
+      es_user                   = var.master_user_name,
+      es_pass                   = var.master_user_password,
+      aws_es_endpoint           = aws_elasticsearch_domain.cluster.endpoint,
+      logstash_username         = var.logstash_username,
+      logstash_password         = var.logstash_password,
+      logstash_permissions      = var.logstash_permissions,
+      proxy_cluster_permissions = var.proxy_cluster_permissions,
+      proxy_username            = var.proxy_username,
+      proxy_password            = var.proxy_password,
+  })
+  filename = "${path.module}/${var.domain_name}-bootstrap-service-script.sh"
 }
 
 resource "null_resource" "create_cluster_indices" {
-  count = var.large_index_list  != [] || var.medium_index_list  != [] || var.small_index_list != [] ? 1 : 0
+  count = var.large_index_list != [] || var.medium_index_list != [] || var.small_index_list != [] ? 1 : 0
 
   triggers = {
-    large_index_list = join(",", var.large_index_list)
+    large_index_list  = join(",", var.large_index_list)
     medium_index_list = join(",", var.medium_index_list)
-    small_index_list = join(",", var.small_index_list)
+    small_index_list  = join(",", var.small_index_list)
   }
 
   provisioner "local-exec" {
-    command = "./${local_file.indices_script[count.index].filename}"
+    command     = "./${local_file.indices_script[count.index].filename}"
     interpreter = ["sh"]
   }
 
@@ -181,38 +181,38 @@ resource "null_resource" "create_cluster_indices" {
 }
 
 resource "local_file" "indices_script" {
-    count = var.large_index_list  != [] || var.medium_index_list  != [] || var.small_index_list != [] ? 1 : 0
-    content  = templatefile("${path.module}/files/create_indices.tmpl", 
-                {
-                  es_user=var.master_user_name,
-                  es_pass=var.master_user_password,
-                  aws_es_endpoint=aws_elasticsearch_domain.cluster.endpoint,
-                  large_index_list=var.large_index_list
-                  large_index_shard_count=var.large_index_shard_count
-                  large_index_shard_count=var.large_index_shard_count
-                  large_index_refresh_interval=var.large_index_refresh_interval
-                  large_index_replica_count=var.large_index_replica_count
-                  large_index_rollover_size=var.large_index_rollover_size
-                  large_index_rollover_age=var.large_index_rollover_age
-                  large_index_retention=var.large_index_retention
-                  medium_index_list=var.medium_index_list
-                  medium_index_shard_count=var.medium_index_shard_count
-                  medium_index_shard_count=var.medium_index_shard_count
-                  medium_index_refresh_interval=var.medium_index_refresh_interval
-                  medium_index_replica_count=var.medium_index_replica_count
-                  medium_index_rollover_size=var.medium_index_rollover_size
-                  medium_index_rollover_age=var.medium_index_rollover_age
-                  medium_index_retention=var.medium_index_retention
-                  small_index_list=var.small_index_list
-                  small_index_shard_count=var.small_index_shard_count
-                  small_index_shard_count=var.small_index_shard_count
-                  small_index_refresh_interval=var.small_index_refresh_interval
-                  small_index_replica_count=var.small_index_replica_count
-                  small_index_rollover_size=var.small_index_rollover_size
-                  small_index_rollover_age=var.small_index_rollover_age
-                  small_index_retention=var.small_index_retention
-                })
-    filename = "${path.module}/${var.domain_name}-cluster-index-script.sh"
+  count = var.large_index_list != [] || var.medium_index_list != [] || var.small_index_list != [] ? 1 : 0
+  content = templatefile("${path.module}/files/create_indices.tmpl",
+    {
+      es_user                       = var.master_user_name,
+      es_pass                       = var.master_user_password,
+      aws_es_endpoint               = aws_elasticsearch_domain.cluster.endpoint,
+      large_index_list              = var.large_index_list
+      large_index_shard_count       = var.large_index_shard_count
+      large_index_shard_count       = var.large_index_shard_count
+      large_index_refresh_interval  = var.large_index_refresh_interval
+      large_index_replica_count     = var.large_index_replica_count
+      large_index_rollover_size     = var.large_index_rollover_size
+      large_index_rollover_age      = var.large_index_rollover_age
+      large_index_retention         = var.large_index_retention
+      medium_index_list             = var.medium_index_list
+      medium_index_shard_count      = var.medium_index_shard_count
+      medium_index_shard_count      = var.medium_index_shard_count
+      medium_index_refresh_interval = var.medium_index_refresh_interval
+      medium_index_replica_count    = var.medium_index_replica_count
+      medium_index_rollover_size    = var.medium_index_rollover_size
+      medium_index_rollover_age     = var.medium_index_rollover_age
+      medium_index_retention        = var.medium_index_retention
+      small_index_list              = var.small_index_list
+      small_index_shard_count       = var.small_index_shard_count
+      small_index_shard_count       = var.small_index_shard_count
+      small_index_refresh_interval  = var.small_index_refresh_interval
+      small_index_replica_count     = var.small_index_replica_count
+      small_index_rollover_size     = var.small_index_rollover_size
+      small_index_rollover_age      = var.small_index_rollover_age
+      small_index_retention         = var.small_index_retention
+  })
+  filename = "${path.module}/${var.domain_name}-cluster-index-script.sh"
 }
 
 resource "null_resource" "create_cluster_tenants" {
@@ -223,7 +223,7 @@ resource "null_resource" "create_cluster_tenants" {
   }
 
   provisioner "local-exec" {
-    command = "./${local_file.tenants_script[count.index].filename}"
+    command     = "./${local_file.tenants_script[count.index].filename}"
     interpreter = ["sh"]
   }
 
@@ -233,13 +233,13 @@ resource "null_resource" "create_cluster_tenants" {
 }
 
 resource "local_file" "tenants_script" {
-    count = var.tenant_list != [] ? 1 : 0
-    content  = templatefile("${path.module}/files/create_tenants.tmpl", 
-                {
-                  es_user=var.master_user_name,
-                  es_pass=var.master_user_password,
-                  aws_es_endpoint=aws_elasticsearch_domain.cluster.endpoint,
-                  tenant_list=var.tenant_list
-                })
-    filename = "${path.module}/${var.domain_name}-tenant-script.sh"
+  count = var.tenant_list != [] ? 1 : 0
+  content = templatefile("${path.module}/files/create_tenants.tmpl",
+    {
+      es_user         = var.master_user_name,
+      es_pass         = var.master_user_password,
+      aws_es_endpoint = aws_elasticsearch_domain.cluster.endpoint,
+      tenant_list     = var.tenant_list
+  })
+  filename = "${path.module}/${var.domain_name}-tenant-script.sh"
 }
--- variables.tf
+++ variables.tf
@@ -1,31 +1,31 @@
 variable "domain_name" {
-  
+
 }
 
 variable "elasticsearch_version" {
-  
+
 }
 
 variable "data_instance_type" {
-  
+
 }
 
 variable "data_instance_count" {
-  
+
 }
 
 variable "master_enabled" {
-  default = false 
+  default     = false
   description = "Boolean value to enable/disable dedicated master nodes."
 }
 
 variable "master_instance_count" {
-  default = 0
+  default     = 0
   description = "The amount of dedicated master nodes within the cluster."
 }
 
 variable "master_instance_type" {
-  default = ""
+  default     = ""
   description = "The instance type for the dedicated master nodes."
 }
 
@@ -34,7 +34,7 @@ variable "automated_snapshot_start_hour" {
 }
 
 variable "tags" {
-  
+
 }
 
 variable "encrypt_at_rest_enabled" {
@@ -42,162 +42,197 @@ variable "encrypt_at_rest_enabled" {
 }
 
 variable "ebs_enabled" {
-    default = false
+  default = false
 }
 
 variable "volume_type" {
-    default = ""
+  default = ""
 }
-    
+
 variable "volume_size" {
-    default = 0
+  default = 0
 }
 
 variable "admin_users" {
   description = "group of users to be created in kibana."
-  default = []
+  default     = []
 }
 
 variable "tenant_list" {
   description = "List of tenant names to be provisioned in Elasticsearch."
-  default = []
+  default     = []
 }
 
 variable "large_index_list" {
   description = "List of larger sized indexes to be created in Elasticsearch. e.g expected 1TB per day"
-  default = []
+  default     = []
 }
 
 variable "large_index_shard_count" {
   description = "Updates the large index template to define the amount of shards set for all indices."
-  default = "12"
+  default     = "12"
 }
 
 variable "large_index_refresh_interval" {
   description = "Updates the large index template to define the interval between each index refresh."
-  default = "30s"
+  default     = "30s"
 }
 
 variable "large_index_replica_count" {
   description = "Updates the large index template to define the amount of replica shards set for all indices."
-  default = "1"
+  default     = "1"
 }
 
 variable "large_index_rollover_size" {
   description = "Updates the large ISM policy with the index size limit before triggering the rollover action."
-  default = "600gb"
+  default     = "600gb"
 }
 
 variable "large_index_rollover_age" {
   description = "Updates the large ISM policy with the index age limit before triggering the rollover action."
-  default = "1d"
+  default     = "1d"
 }
 
 variable "large_index_retention" {
   description = "Updates the ISM policy with the index retention time before being deleted."
-  default = "7d"
+  default     = "7d"
 }
 
 variable "medium_index_list" {
   description = "List of medium sized indexes to be created in Elasticsearch. e.g 30GB per day"
-  default = []
+  default     = []
 }
 
 variable "medium_index_shard_count" {
   description = "Updates the medium index template to define the amount of shards set for all indices."
-  default = "6"
+  default     = "6"
 }
 
 variable "medium_index_refresh_interval" {
   description = "Updates the medium index template to define the interval between each index refresh."
-  default = "30s"
+  default     = "30s"
 }
 
 variable "medium_index_replica_count" {
   description = "Updates the medium index template to define the amount of replica shards set for all indices."
-  default = "1"
+  default     = "1"
 }
 
 variable "medium_index_rollover_size" {
   description = "Updates the medium ISM policy with the index size limit before triggering the rollover action."
-  default = "300gb"
+  default     = "300gb"
 }
 
 variable "medium_index_rollover_age" {
   description = "Updates the medium ISM policy with the index age limit before triggering the rollover action."
-  default = "1d"
+  default     = "1d"
 }
 
 variable "medium_index_retention" {
   description = "Updates the medium ISM policy with the index retention time before being deleted."
-  default = "7d"
+  default     = "7d"
 }
 
 variable "small_index_list" {
   description = "List of small sized indexes to be created in Elasticsearch. e.g expected 5GB per day"
-  default = []
+  default     = []
 }
 
 variable "small_index_shard_count" {
   description = "Updates the small index template to define the amount of shards set for all indices."
-  default = "1"
+  default     = "1"
 }
 
 variable "small_index_refresh_interval" {
   description = "Updates the small index template to define the interval between each index refresh."
-  default = "30s"
+  default     = "30s"
 }
 
 variable "small_index_replica_count" {
   description = "Updates the small index template to define the amount of replica shards set for all indices."
-  default = "1"
+  default     = "1"
 }
 
 variable "small_index_rollover_size" {
   description = "Updates the small ISM policy with the index size limit before triggering the rollover action."
-  default = "30gb"
+  default     = "30gb"
 }
 
 variable "small_index_rollover_age" {
   description = "Updates the small ISM policy with the index age limit before triggering the rollover action."
-  default = "1d"
+  default     = "1d"
 }
 
 variable "small_index_retention" {
   description = "Updates the ISM policy with the index retention time before being deleted."
-  default = "7d"
+  default     = "7d"
 }
 
 variable "logstash_username" {
   description = "Logstash username in Kibana RBAC"
-  default = "logstash-acp"
+  default     = "_logstash-acp"
 }
 
 variable "logstash_password" {
   description = "Logstash password in Kibana RBAC"
 }
 
-variable "logstash_permissions" {
-  description = "Logstash index level permissions. Due to predefined permissions in template, this requires escaped double quotes followed by a trailing comma for JSON object e.g \"read\","
-  default = ""
+variable "logstash_index_permissions" {
+  description = "Logstash index-level permissions."
+  default = [
+    "read",
+    "get",
+    "indices:data/write/bulk",
+    "indices:data/write/bulk*",
+    "indices:data/write/index",
+    "indices:admin/mapping/put",
+    "indices:monitor/settings/get",
+    "indices:monitor/stats"
+  ]
 }
+
+variable "logstash_cluster_permissions" {
+  description = "Logstash cluster-level permissions."
+  default = [
+    "cluster:admin/ingest/pipeline/get",
+    "cluster:admin/ingest/pipeline/put",
+    "cluster_monitor",
+    "indices:data/write/bulk",
+    "indices:data/write/bulk*",
+    "indices:data/write/index",
+    "indices:admin/mapping/put"
+  ]
+}
+
+variable "proxy_user_toggle" {
+  description = "Toggle whether or not to create the proxy user"
+  default     = false
+}
+
 
 variable "proxy_username" {
   description = "Proxy username in Kibana RBAC"
-  default = "auth-proxy"
+  default     = "_auth-proxy"
 }
 
 variable "proxy_password" {
   description = "Proxy password in Kibana RBAC"
+  default = ""
 }
 
 variable "proxy_cluster_permissions" {
   description = "Proxy cluster-level permissions"
-  default = ""
+  default     = []
 }
 
+variable "proxy_cluster_permissions" {
+  description = "Proxy index-level permissions"
+  default     = []
+}
+
+
 variable "master_user_name" {
-  
+
 }
 
 variable "master_user_password" {
@@ -205,11 +240,11 @@ variable "master_user_password" {
 }
 
 variable "vpc_id" {
-  
+
 }
 
 variable "subnet_ids" {
-  
+
 }
 
 variable "sg_ingress_cidr_blocks" {

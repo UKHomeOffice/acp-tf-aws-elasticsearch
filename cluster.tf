b47ed2bccbf4f6e56adc73d7b14752d1f0d95c77
Alastair Hinde <alastairhinde@Alastairs-MacBook-Pro.local>
2021-09-21 22:52:29 +0100

    removed local files to use null resources to create local files. This will ensure clean terraform plan outputs and not render files during each terraform apply

--- cluster.tf
+++ cluster.tf
@@ -106,143 +106,110 @@ resource "time_sleep" "wait_60_seconds" {
 
 resource "null_resource" "create_local_users" {
   count = var.admin_users != [] ? 1 : 0
-
   triggers = {
     users = join(",", var.admin_users)
   }
-
   provisioner "local-exec" {
-    command     = "./${local_file.script[count.index].filename}"
+    command     = "./${path.module}/${var.domain_name}-admin-user-script.sh"
     interpreter = ["sh"]
   }
-
   depends_on = [
     time_sleep.wait_60_seconds
   ]
 }
 
-resource "local_file" "script" {
+resource "null_resource" "admin_file" {
   count = var.admin_users != [] ? 1 : 0
-  content = templatefile("${path.module}/files/create_admin_users.tmpl",
-    {
-      es_user         = var.master_user_name,
-      es_pass         = var.master_user_password,
-      aws_es_endpoint = aws_elasticsearch_domain.cluster.endpoint,
-      admin_users     = var.admin_users
-  })
-  filename = "${path.module}/${var.domain_name}-admin-user-script.sh"
+  triggers = {
+    admin_users = join(",", var.admin_users)
+  }
+  provisioner "local-exec" {
+    command = format(
+      "cat <<\"EOF\" > \"%s\"\n%s\nEOF",
+      "${path.module}/${var.domain_name}-admin-user-script.sh",
+      local.admin_content
+    )
+  }
 }
 
 resource "null_resource" "bootstrap_service_users" {
-
   provisioner "local-exec" {
-    command     = "./${local_file.service_users_script.filename}"
+    command     = "./${path.module}/${var.domain_name}-bootstrap-service-script.sh"
     interpreter = ["sh"]
   }
-
   depends_on = [
     time_sleep.wait_60_seconds
   ]
 }
 
-resource "local_file" "service_users_script" {
-  content = templatefile("${path.module}/files/bootstrap_service_users.tmpl",
-    {
-      es_user                      = var.master_user_name,
-      es_pass                      = var.master_user_password,
-      aws_es_endpoint              = aws_elasticsearch_domain.cluster.endpoint,
-      logstash_username            = var.logstash_username,
-      logstash_password            = var.logstash_password,
-      logstash_cluster_permissions = var.logstash_cluster_permissions,
-      logstash_index_permissions   = var.logstash_index_permissions,
-      proxy_cluster_permissions    = var.proxy_cluster_permissions,
-      proxy_username               = var.proxy_username,
-      proxy_password               = var.proxy_password,
-      proxy_cluster_permissions    = var.logstash_cluster_permissions,
-      proxy_index_permissions      = var.logstash_index_permissions,
-  })
-  filename = "${path.module}/${var.domain_name}-bootstrap-service-script.sh"
+resource "null_resource" "service_user_file" {
+  triggers = {
+    logstash_username = var.logstash_username
+    proxy_username = var.proxy_username
+  }
+  provisioner "local-exec" {
+    command = format(
+      "cat <<\"EOF\" > \"%s\"\n%s\nEOF",
+      "${path.module}/${var.domain_name}-bootstrap-service-script.sh",
+      local.service_user_content
+    )
+  }
 }
 
 resource "null_resource" "create_cluster_indices" {
   count = var.large_index_list != [] || var.medium_index_list != [] || var.small_index_list != [] ? 1 : 0
-
   triggers = {
     large_index_list  = join(",", var.large_index_list)
     medium_index_list = join(",", var.medium_index_list)
     small_index_list  = join(",", var.small_index_list)
   }
-
   provisioner "local-exec" {
-    command     = "./${local_file.indices_script[count.index].filename}"
+    command     = "./${path.module}/${var.domain_name}-cluster-index-script.sh"
     interpreter = ["sh"]
   }
-
   depends_on = [
     time_sleep.wait_60_seconds
   ]
 }
 
-resource "local_file" "indices_script" {
+resource "null_resource" "indices_file" {
   count = var.large_index_list != [] || var.medium_index_list != [] || var.small_index_list != [] ? 1 : 0
-  content = templatefile("${path.module}/files/create_indices.tmpl",
-    {
-      es_user                       = var.master_user_name,
-      es_pass                       = var.master_user_password,
-      aws_es_endpoint               = aws_elasticsearch_domain.cluster.endpoint,
-      large_index_list              = var.large_index_list
-      large_index_shard_count       = var.large_index_shard_count
-      large_index_shard_count       = var.large_index_shard_count
-      large_index_refresh_interval  = var.large_index_refresh_interval
-      large_index_replica_count     = var.large_index_replica_count
-      large_index_rollover_size     = var.large_index_rollover_size
-      large_index_rollover_age      = var.large_index_rollover_age
-      large_index_retention         = var.large_index_retention
-      medium_index_list             = var.medium_index_list
-      medium_index_shard_count      = var.medium_index_shard_count
-      medium_index_shard_count      = var.medium_index_shard_count
-      medium_index_refresh_interval = var.medium_index_refresh_interval
-      medium_index_replica_count    = var.medium_index_replica_count
-      medium_index_rollover_size    = var.medium_index_rollover_size
-      medium_index_rollover_age     = var.medium_index_rollover_age
-      medium_index_retention        = var.medium_index_retention
-      small_index_list              = var.small_index_list
-      small_index_shard_count       = var.small_index_shard_count
-      small_index_shard_count       = var.small_index_shard_count
-      small_index_refresh_interval  = var.small_index_refresh_interval
-      small_index_replica_count     = var.small_index_replica_count
-      small_index_rollover_size     = var.small_index_rollover_size
-      small_index_rollover_age      = var.small_index_rollover_age
-      small_index_retention         = var.small_index_retention
-  })
-  filename = "${path.module}/${var.domain_name}-cluster-index-script.sh"
+  triggers = {
+    template = local.index_content
+  }
+  provisioner "local-exec" {
+    command = format(
+      "cat <<\"EOF\" > \"%s\"\n%s\nEOF",
+      "${path.module}/${var.domain_name}-cluster-index-script.sh",
+      local.index_content
+    )
+  }
 }
 
 resource "null_resource" "create_cluster_tenants" {
   count = var.tenant_list != [] ? 1 : 0
-
   triggers = {
     tenant_list = join(",", var.tenant_list)
   }
-
   provisioner "local-exec" {
-    command     = "./${local_file.tenants_script[count.index].filename}"
+    command     = "./${path.module}/${var.domain_name}-tenant-script.sh"
     interpreter = ["sh"]
   }
-
   depends_on = [
     time_sleep.wait_60_seconds
   ]
 }
 
-resource "local_file" "tenants_script" {
+resource "null_resource" "tenant_file" {
   count = var.tenant_list != [] ? 1 : 0
-  content = templatefile("${path.module}/files/create_tenants.tmpl",
-    {
-      es_user         = var.master_user_name,
-      es_pass         = var.master_user_password,
-      aws_es_endpoint = aws_elasticsearch_domain.cluster.endpoint,
-      tenant_list     = var.tenant_list
-  })
-  filename = "${path.module}/${var.domain_name}-tenant-script.sh"
+  triggers = {
+    template = local.tenant_content
+  }
+  provisioner "local-exec" {
+    command = format(
+      "cat <<\"EOF\" > \"%s\"\n%s\nEOF",
+      "${path.module}/${var.domain_name}-tenant-script.sh",
+      local.tenant_content
+    )
+  }
 }
